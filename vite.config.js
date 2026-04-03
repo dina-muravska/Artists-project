535def078bac8f4b480e561b7cec00e39c8b7333
Dina Muravska <dina.muravska.25@gmail.com>
2026-04-03 19:53:32 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+
 export default defineConfig(({ command }) => {
   return {
-    base: '/Artists-project/',
-
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-
     root: 'src',
     build: {
       sourcemap: true,
